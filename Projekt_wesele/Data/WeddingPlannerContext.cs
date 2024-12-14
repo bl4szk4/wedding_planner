8eb51aefed6165c415997f118174c3ace72bdd03
bl4szk4 <blachabartlomiej@gmail.com>
2024-12-14 15:42:55 +0100

    Dodaj pliki projektów.

---
+++ Projekt_wesele/Data/WeddingPlannerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projekt_wesele.Models;
+
+namespace Projekt_wesele.Data
+{
+    public class WeddingPlannerContext
+    {
+        public DbSet<Guest> Gests { get; set; }
+        public DbSet<BudgetItem> BudgetItems { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ Projekt_wesele/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_wesele.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Attending { get; set; }
+        public string? Notes { get; set; }
+    }
+}
